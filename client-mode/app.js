3220db7ad127de299edfb95a7f918b0c4b95d8a0
1019940946 <1019940946@qq.com>
2021-12-17 02:45:59 +0800

    Update app.js
    改成IPV6模式

--- client-mode/app.js
+++ client-mode/app.js
@@ -12,23 +12,78 @@ const simpleGetPubIpUrl = [
 
 const taobaoApi = {
     option: {
-        host: "ip.taobao.com",
-        path: "/service/getIpInfo2.php?ip=myip",
+        host: "v6.ident.me",
+        path: "/",
         method: "POST"
     },
     parser: body => {
-        const j = JSON.parse(body);
-        return j.data.ip;
+        return body;
     }
 }
 
-function isValidIp(ip) {
-    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip)) {
-        return true;
-    } else {
-        return false;
-    }
-}
+function isValidIp(tmpstr)  
+{  
+        //CDCD:910A:2222:5498:8475:1111:3900:2020   
+        var patrn=/^([0-9a-f]{1,4}:){7}[0-9a-f]{1,4}$/i;   
+        var r=patrn.exec(tmpstr)  
+        if(r)  
+        {  
+            return true;  
+        }  
+        if(tmpstr=="::"){  
+            return true;  
+        }  
+        //F:F:F::1:1 F:F:F:F:F::1 F::F:F:F:F:1格式  
+        patrn=/^(([0-9a-f]{1,4}:){0,6})((:[0-9a-f]{1,4}){0,6})$/i;   
+        r=patrn.exec(tmpstr);  
+        if(r)  
+        {      
+            var c=cLength(tmpstr);  
+            if(c<=7 && c>0)  
+            {  
+                return true;  
+            }  
+        }                  
+        //F:F:10F::  
+        patrn=/^([0-9a-f]{1,4}:){1,7}:$/i;   
+        r=patrn.exec(tmpstr);  
+        if(r)  
+        {  
+            return true;  
+        }  
+        //::F:F:10F  
+        patrn=/^:(:[0-9a-f]{1,4}){1,7}$/i;   
+        r=patrn.exec(tmpstr);  
+        if(r)  
+        {  
+            return true;  
+        }  
+        //F:0:0:0:0:0:10.0.0.1格式  
+        patrn=/^([0-9a-f]{1,4}:){6}(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/i;   
+        r=patrn.exec(tmpstr);  
+        if(r)  
+        {  
+            if(r[2]<=255 && r[3]<=255 &&r[4]<=255 && r[5]<=255 )  
+            return true;  
+        }  
+        //F::10.0.0.1格式  
+        patrn=/^([0-9a-f]{1,4}:){1,5}:(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/i;   
+        r=patrn.exec(tmpstr);  
+        if(r)  
+        {  
+            if(r[2]<=255 && r[3]<=255 &&r[4]<=255 && r[5]<=255 )  
+                return true;  
+        }  
+        //::10.0.0.1格式  
+        patrn=/^::(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/i;   
+        r=patrn.exec(tmpstr);  
+        if(r)  
+        {  
+            if(r[1]<=255 && r[2]<=255 &&r[3]<=255 && r[4]<=255)  
+                return true;  
+        }  
+        return false;  
+}  
 
 function getPubIpGeneric(option, parser, callback) {
     const req = http.request(option, res => {
